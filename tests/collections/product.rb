dd851db92dbda4230e33ec0d657eea205040d2ce
edddjunior <edsonbergamojunior@gmail.com>
2019-07-02 14:09:49 -0300

    all method added

--- tests/collections/product.rb
+++ tests/collections/product.rb
@@ -1,4 +1,5 @@
 class Product
+  @@products = []
   attr_reader :id, :name, :quantity, :price
 
   def initialize(data={})
@@ -6,5 +7,10 @@ class Product
     @name = data[:name] || "Test Product"
     @quantity = data[:quantity] || 0
     @price = data[:price] || 0
+    @@products << self
+  end
+
+  def self.all
+    @@products
   end
 end
