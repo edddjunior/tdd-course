bceee10eecffe2dac82ee6580ace10f4ad6b7ccf
edddjunior <edsonbergamojunior@gmail.com>
2019-07-02 14:17:36 -0300

    testing if it returns a list of all product names

--- tests/collections/product_spec.rb
+++ tests/collections/product_spec.rb
@@ -33,4 +33,8 @@ RSpec.describe Product do
   it "returns a list of all products" do
     expect(Product.all).to eq([@p1])
   end
+
+  it "returns a list of all product names" do
+    expect(Product.product_names).to eq([@p1.name])
+  end
 end
