0f8acb0bb472776db6157c48a4fc3659ddd95ba1
edddjunior <edsonbergamojunior@gmail.com>
2019-07-16 16:54:51 -0300

    deck runner completed

--- tests/blackjack/deck_runner.rb
+++ tests/blackjack/deck_runner.rb
@@ -1,1 +1,25 @@
 require_relative 'deck'
+
+SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+deck = Deck.new(SUITS, RANKS)
+puts "Deck:"
+puts deck.shuffle
+
+puts
+
+puts "Dealt card:"
+card = deck.deal_card
+puts card
+
+puts
+
+deck_of_cards = []
+deck_of_cards.push(Card.new("Spades", "8"))
+deck_of_cards.push(Card.new("Hearts", "8"))
+deck_of_cards.push(Card.new("Diamonds", "8"))
+
+deck.replace_with deck_of_cards
+puts "New deck of cards:"
+puts deck.shuffle
