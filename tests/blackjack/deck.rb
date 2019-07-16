a1a524ab2d0e331d39815154a2eb6f747ad7384c
edddjunior <edsonbergamojunior@gmail.com>
2019-07-16 15:39:29 -0300

    making deck specs pass

---
+++ tests/blackjack/deck.rb
@@ -0,0 +1,37 @@
+require_relative 'card'
+require_relative 'deck'
+
+class Deck
+  SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+  RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+  attr_reader :deck, :suits, :ranks
+
+  def initialize(suits, ranks)
+    @deck = []
+    @suits = suits
+    @ranks = ranks
+    create_deck
+  end
+
+  def shuffle
+    @deck.shuffle!
+  end
+
+  def deal_card
+    @deck.pop
+  end
+
+  def replace_with
+    
+  end
+
+  private
+  def create_deck
+    suits.each do |suit|
+      ranks.each do |rank|
+        @deck.push(Card.new(suit, rank))
+      end
+    end
+  end
+end
